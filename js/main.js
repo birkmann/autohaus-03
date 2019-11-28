401c0c459ea68db387e8ef878f9e9f7f7e7c56fe
Rafael Birkmann <mail@rafaelbirkmann.de>
2019-11-28 22:18:59 +0100

    files

--- js/main.js
+++ js/main.js
@@ -10,14 +10,15 @@ $(document).ready(function() {
 	$(".emotion .slides").slick({
 		dots: true,
 		infinite: true,
-		autoplay: true,
+		autoplay: false,
+		arrows: false,
 		autoplaySpeed: 5000,
 		cssEase: 'linear'
 	});
 });
 
 $(window).scroll(function(){
-	if ( $(this).scrollTop() > 200 ) { 
+	if ( $(this).scrollTop() > 100 ) { 
 		$('body').addClass("scrolled");
 	} else { 
 		$('body').removeClass("scrolled");
